3d77fcdf8cafde465a294b96b41f0f61d0d752e4
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2021-03-09 14:41:19 +0200

    Backend/Route created to create user profile and get current profile

---
+++ models/Profile.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  bio: {
+    type: String,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  pastExperience: [
+    {
+      title: {
+        type: String,
+        required: true,
+      },
+      location: {
+        type: String,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  qualification: [
+    {
+      degree: {
+        type: String,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  info: {
+    phone: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
