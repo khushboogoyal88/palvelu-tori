4f5af4343f3b0c855b7facbba8ce568ca12c599e
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2021-03-12 13:45:54 +0200

    Frontend/Sign In and Register page ready

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
-import React, {Fragment, useState} from 'react'
+import React, {Fragment, useState} from 'react';
+import {Link} from "react-router-dom"
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -12,12 +13,12 @@ const Register = () => {
     
     const changeHandler = e => setFormData({...formData, [e.target.name]: e.target.value})
     
-    const submitHandler = e =>{
+    const submitHandler = async e =>{
         e.preventDefault();
         if(password !== password2){
             console.log('Passwords do not match');
         }else{
-            console.log(formData);
+            console.log('Success');
         }
     }
     return (
@@ -47,8 +48,7 @@ const Register = () => {
               required
             />
             <small className='form-text'>
-              This site uses Gravatar so if you want a profile image, use a
-              Gravatar email
+              
             </small>
           </div>
           <div className='form-group'>
@@ -76,7 +76,7 @@ const Register = () => {
           <input type='submit' className='btn btn-primary' value='Register' />
         </form>
         <p className='my-1'>
-          Already have an account? <a href='login.html'>Sign In</a>
+          Already have an account? <Link to='/login'>Sign In</Link>
         </p>
       </Fragment>
     );
